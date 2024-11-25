d4e492eb5abbac03e61ea7fe29e58bac1e5f594a
SIVI231906 <sivaranjani.hhc@gmail.com>
2024-11-25 10:42:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     github = {
-      source = "integrations/github"
+      source  = "integrations/github"
       version = "~> 4.0"
     }
   }
 }
 
 variable "github_token" {
-  description = "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"
+  description = "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" 
   type        = string
   sensitive   = true
 }
@@ -17,10 +17,10 @@ provider "github" {
   token = var.github_token
 }
 
-
 resource "github_repository" "devops_project_2023188027" {
   name        = "devops-project-2023188027"
   description = "A Terraform-managed DevOps project"
   visibility  = "public"
 }
+
 
