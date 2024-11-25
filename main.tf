85b6cf1cba7637637e3207bfd448e23176ff4f76
SIVI231906 <sivaranjani.hhc@gmail.com>
2024-11-25 10:08:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "github" {
-  token = "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"
+  token = "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"
 }
 
 resource "github_repository" "devops_project_2023188027" {
